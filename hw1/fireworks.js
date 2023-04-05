965da7e86ba6d945a6cebcb9eed21c0f895460b1
ali269 <ali.sbirami.132@gmail.com>
2023-04-05 03:30:29 +0330

    implement whistle strike

--- hw1/fireworks.js
+++ hw1/fireworks.js
@@ -1,5 +1,6 @@
-let systems = [];
-
+let explosions = [];
+let whistles = [];
+let test_whistle;
 let explode;
 
 const colors = [
@@ -17,28 +18,52 @@ const colors = [
 function setup() {
   createCanvas(1200, 812);
   explode = loadSound("./sound/explode.mp3");
+  test_whistle = new Whistle(createVector(500, 500));
 }
 
-// events
-function mouseClicked() {
-  let system = new ParticleSystem(createVector(mouseX, mouseY));
+// Actions
+function explosion(position) {
+  let system = new ParticleSystem(position);
   system.fire();
   explode.play();
-  systems.push(system);
+  explosions.push(system);
+}
+
+function strikeWhistle(position) {
+  let whistle = new Whistle(position);
+  whistles.push(whistle);
+}
+
+// Events
+function mousePressed() {
+  // if (mouseY > 500) {
+  //   strikeWhistle(createVector(mouseX, mouseY));
+  // }
+  test_whistle = new Whistle(createVector(mouseX, mouseY));
+}
+
+function mouseReleased() {
+  test_whistle.powerOff();
 }
 
 function draw() {
   background(51);
-  // system.addParticle();
-  for (let i = 0; i < systems.length; i++) {
-    systems[i].run();
+  test_whistle.run();
+  for (let i = 0; i < whistles.length; i++) {
+    whistles[i].run();
+  }
+
+  for (let i = 0; i < explosions.length; i++) {
+    explosions[i].run();
   }
 }
 
-// A simple Particle class
-let Particle = function(position) {
+// Classes
+
+//#region A simple Particle
+let Particle = function(position, initVelocity) {
   this.acceleration = createVector(0, 0.05);
-  this.velocity = createVector(random(-2, 2), random(-2, 2));
+  this.velocity = initVelocity;
   this.position = position.copy();
   this.color = colors[parseInt(random(0, colors.length))];
   this.lifespan = 255;
@@ -72,6 +97,9 @@ Particle.prototype.isDead = function(){
   return this.lifespan < 0;
 };
 
+//#endregion
+
+//#region A simple Particle System
 let ParticleSystem = function(position) {
   this.generate = 50;
   this.origin = position.copy();
@@ -80,14 +108,13 @@ let ParticleSystem = function(position) {
 
 
 ParticleSystem.prototype.addParticle = function() {  
-  this.particles.push(new Particle(this.origin));
+  this.particles.push(new Particle(this.origin, createVector(random(-2, 2), random(-2, 2))));
 };
 
 ParticleSystem.prototype.fire = function() {
   for (let i = 0; i < this.generate; i++) {
     this.addParticle();
   }
-  // explode.play();
 }
 
 ParticleSystem.prototype.run = function() {
@@ -99,3 +126,48 @@ ParticleSystem.prototype.run = function() {
     }
   }
 };
+//#endregion
+
+//#region 
+let Whistle = function(position) {
+  this.acceleration = createVector(0, 0.05);
+  this.velocity = createVector(random(-1, 1), random(-2, -4));
+  this.position = position.copy();
+  this.color = colors[parseInt(random(0, 8))];
+  this.haspower = true;
+}
+
+Whistle.prototype.powerOff = function() {
+  this.haspower = false;
+}
+
+Whistle.prototype.run = function() {
+  this.update();
+  this.display();
+}
+
+Whistle.prototype.update = function() {
+  if (!this.haspower) {
+    this.velocity.add(this.acceleration);
+  }
+  this.position.add(this.velocity);
+  this.lifetime += 1;
+}
+
+Whistle.prototype.display = function() {
+  stroke(200);
+  strokeWeight(0);
+  const [r, g, b] = this.color;
+  console.log(r, g, b);
+  fill(`rgb(${r}, ${g}, ${b})`);
+  ellipse(this.position.x, this.position.y, 6, 6);
+  ellipse(this.position.x - this.velocity.x, 
+    this.position.y - this.velocity.y, 5, 5);
+  ellipse(this.position.x - 2 * this.velocity.x, 
+    this.position.y - 2 * this.velocity.y, 4, 4);
+  ellipse(this.position.x - 3 * this.velocity.x, 
+    this.position.y - 3 * this.velocity.y, 3, 3);
+  ellipse(this.position.x - 4 * this.velocity.x, 
+    this.position.y - 4 * this.velocity.y, 2, 2);
+}
+//#endregion
---
+++ hw1/tasks.md
@@ -0,0 +1,7 @@
+# todo list
+2. implement action chain
+3. implement key events
+4. implement mouse event
+
+# done
+1. implement whistle strike
