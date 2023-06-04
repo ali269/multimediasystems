cdf482de7f0109ee906d5014f6cc3fd1a78e983a
alireza <ali.sbirami.132@gmail.com>
2023-06-04 17:19:08 +0330

    feat: step 2 completed

--- hist_norm.py
+++ hist_norm.py
@@ -21,9 +21,18 @@ def main(args):
         src_data[:,:,2] = src_img[:,:,2] * src_img[:,:,3]
     else:
         src_data = src_img
-    gray_img = (src_data[:,:,0] + src_data[:,:,1] + src_data[:,:,2]) * 255 /3
+    # convert rgb to grayscale color
+    gray_img = np.ndarray.astype((src_data[:,:,0] + src_data[:,:,1] + src_data[:,:,2]) * 255 /3, dtype=np.uint8)
     plt.imsave("gray_img.png", gray_img, cmap="gray")
 
+    # create color histogram
+    color_hist = [0 for i in range(256)]
+    for g_code in gray_img.flatten():
+        color_hist[g_code] += 1
+    print(color_hist[100])
+    plt.hist(gray_img.flatten().tolist(), list(range(256)))
+    plt.savefig("b_norm_color_hist.png")
+    
 
 if __name__=='__main__':
     args = parser.parse_args()
